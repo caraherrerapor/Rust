bca31a5e89f62cd06b8c608c82fa3f97703a4863
carlos <caraherrerapor@gmail.com>
2021-01-17 20:45:02 -0500

    formatted print

---
+++ formatted_print.rs
@@ -0,0 +1,21 @@
+fn main(){
+    println!("{} days",31);
+
+    println!("{0},this is {1}. {1} this is {0}","alice","bob");
+
+    println!("{subject}{verb}{object}",
+             object = "the lazy",
+             subject = "the quick brown fox",
+             verb = "jumps over");
+    println!("{} of {:b} people know binary the other half doesn't",1,2);    
+    
+    println!("{number:>0width$}",number = 1,width = 10);
+
+    println!("{number:>width$}",number = 1, width = 10);
+
+    println!("My name is {0},{1} {0}","bond","james");
+
+    //struct Structure(i32);
+    
+    println!("This struct `{}` won't prin ...",Structure(3));
+}
